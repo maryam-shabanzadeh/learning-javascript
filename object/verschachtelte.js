b144d8386badb57bfeeeaa92f159898b61d4fc82
maryam <maryam.shabanzadeh@valinton.com>
2022-12-04 17:15:29 +0100

    update-verschachtelte

--- object/verschachtelte.js
+++ object/verschachtelte.js
@@ -4,28 +4,36 @@ let bic="WEASEPX";
 let Kontostand=3500;
 let aktive=true;
 */
-let inhaber_1 = {
-    vorname:"maryam",
-    nachname:"shabanzadeh",
-    geschlecht:"femels",
-    alter:37
+// let inhaber_1 = {
+//     vorname:"maryam",
+//     nachname:"shabanzadeh",
+//     geschlecht:"femels",
+//     alter:37
 
 
-};
+// };
 
-console.log(inhaber_1);
+// console.log(inhaber_1);
 
-let inhaber_2 = {
-    vorname: "Hanie",
-    nachname: "mahsomnia",
-    geschlecht: "femels",
-    alter: 16
-};
-console.log(inhaber_2);
+// let inhaber_2 = {
+//     vorname: "Hanie",
+//     nachname: "mahsomnia",
+//     geschlecht: "femels",
+//     alter: 16
+// };
+
 let konto_1 = {
     iban: "DE623458723",
     bic:"WEASEPX",
-    inhaber: inhaber_1,
+    inhaber: {
+
+        vorname:"maryam",
+
+        nachname:"shabanzadeh",
+        geschlecht:"femels",
+        alter:37
+
+    },
     Kontostand:3500,
     aktive:true
 
@@ -35,7 +43,12 @@ let konto_1 = {
 let konto_2 = {
     iban: "DE623458723",
     bic:"WEASEPX",
-    inhaber:inhaber_2,
+    inhaber:{
+        vorname: "Hanie",
+        nachname: "mahsomnia",
+        geschlecht: "femels",
+        alter: 16
+    },
     Kontostand:3500,
     aktive:true
 
@@ -44,5 +57,23 @@ let konto_2 = {
 };
 
 
-console.log(konto_1);
+//console.log(konto_1);
 //INhaber-object(vorname, nachname, Geschlechte, Alter)
+//object füt ein Auto anlegen:
+
+let AUto ={
+    Marke:"BMV",
+    Model:"5d",
+    Kilometerstand:0,
+    Ausstaturng:{
+        Navigationssystem:true,
+        kilmeananlagen:true,
+        sitzheizung:true,
+        tempormadach:false,
+        panoramadach:false,
+    },
+    zustand:"metal",
+    preise:10000
+
+};
+console.log(AUto)
