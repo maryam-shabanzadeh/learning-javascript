fce0673da7fc246ebb884dfc41a16e33ba73811c
maryam <maryam.shabanzadeh@valinton.com>
2022-12-04 17:12:50 +0100

    verschalte_obj

---
+++ object/verschachtelte.js
@@ -0,0 +1,48 @@
+/*
+let iban="DE623458723";
+let bic="WEASEPX";
+let Kontostand=3500;
+let aktive=true;
+*/
+let inhaber_1 = {
+    vorname:"maryam",
+    nachname:"shabanzadeh",
+    geschlecht:"femels",
+    alter:37
+
+
+};
+
+console.log(inhaber_1);
+
+let inhaber_2 = {
+    vorname: "Hanie",
+    nachname: "mahsomnia",
+    geschlecht: "femels",
+    alter: 16
+};
+console.log(inhaber_2);
+let konto_1 = {
+    iban: "DE623458723",
+    bic:"WEASEPX",
+    inhaber: inhaber_1,
+    Kontostand:3500,
+    aktive:true
+
+
+
+};
+let konto_2 = {
+    iban: "DE623458723",
+    bic:"WEASEPX",
+    inhaber:inhaber_2,
+    Kontostand:3500,
+    aktive:true
+
+
+
+};
+
+
+console.log(konto_1);
+//INhaber-object(vorname, nachname, Geschlechte, Alter)
