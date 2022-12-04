3a266964a9fc1d5502e0ab28ed8df071763580b7
maryam <maryam.shabanzadeh@valinton.com>
2022-12-04 16:21:47 +0100

    groundlage_obj_1

---
+++ object/groundlage_obj.js
@@ -0,0 +1,18 @@
+/*
+let iban="DE623458723";
+let bic="WEASEPX";
+let Kontostand=3500;
+let aktive=true;
+*/
+
+let konto_1 = {
+    iban: "DE623458723",
+    bic:"WEASEPX",
+    Kontostand:3500,
+    aktive:true
+
+
+
+};
+
+console.log(konto_1);
--- udemy-projekt/js/main.js
+++ udemy-projekt/js/main.js
@@ -1,61 +1,79 @@
 "use strict";
-let einnahmen =0;
-let ausgaben =0;
-let bilanz =0;
+
 
 const prompt = require("prompt-sync")();
+let einnahmen =0, ausgaben =0, bilanz =0;
 
-let titel_1, type_1,betrag_1,datum_1;
+let titel, type,betrag,datum;
 
-function eintrag_erfassen(titel_1,titel_2,betrag_1,betrag_2){
-    titel_1 = prompt("Titel: ");
-    type_1 = prompt("Type(Einnahme oder ausgaben: ");
-    betrag_1 = prompt(parseInt("Betrag (in cent): "));
-    datum_1 = prompt("Datum(jjj-mm-tt)");
+const eintrag_erfassen=function(){
+    titel = prompt("Titel: ");
+    type = prompt("Type(Einnahme oder ausgaben): ");
+    betrag = parseInt(prompt("Betrag (in cent): "));
+    datum = prompt("Datum(jjj-mm-tt)");
+};
 
+const eintrag_ausgaben = function(titel,type,betrag,datum){
+    console.log(`Titel: ${titel}
+Type: ${type}
+Betrag: ${betrag} ct
+Datum: ${datum} `
+);
+
+};
+
+
+const eintag_mit_gesamtbilanz = function(type, betrag){
+    if (type == "Einnahme"){
+        einnahmen+=betrag;
+        bilanz = bilanz + betrag;
+    }
+    else{
+        ausgaben+=betrag;
+        betrag -=betrag;
+    }
+
+};
+
+
+// // console.log(`
+// // Titel: ${titel_2}
+// // Type: ${type_2}
+// // Betrag: ${betrag_2}
+// // Datum: ${datum_2}`);
+
+// if (betrag_2== "Einnahme"){
+//     einnahmen+=type_2;
+// }
+// else{
+//     ausgaben+=type_2;
+// }
+
+
+const ausgeben_gesamtbilanz = function (einnahmen,ausgaben,bilanz) { 
     
-}
+    let positive = bilanz>=0;
+    console.log(
+        `Einnahmen: ${einnahmen} ct
+        Ausgaben: ${ausgaben} ct
+        Bilanz: ${bilanz>=0}`
+    
+    );
 
-console.log(`Type: ${titel_1}
-Type: ${type_1}
-Betrag: ${betrag_1}
-Datum: ${datum_1} `
-);
-
-if (type_1 == "Einnahme"){
-    einnahmen+=betrag_1;
-    bilanz = bilanz + betrag_1;
-}
-else{
-    ausgaben+=betrag_1
-}
-let titel_2 = prompt("Titel: ");
+   
+    
+};
 
 
-let type_2 = prompt("Type(Einnahme oder ausgaben: ");
 
-let betrag_2 = parseInt(prompt("Betrag (in cent): "));
+const gesamt_auszufuhren = function(){
+    eintrag_erfassen();
+    eintrag_ausgaben(titel,type,betrag,datum);
+    eintag_mit_gesamtbilanz(type,betrag);
+    ausgeben_gesamtbilanz(einnahmen,ausgaben,bilanz);
+    
 
 
-let datum_2 = prompt("Datum(jjj-mm-tt): ");
 
-console.log(`
-Titel: ${titel_2}
-Type: ${type_2}
-Betrag: ${betrag_2}
-Datum: ${datum_2}`);
-
-if (betrag_2== "Einnahme"){
-    einnahmen+=type_2;
-}
-else{
-    ausgaben+=type_2;
-}
-let positive = bilanz >=0;
-//Gesamtbilanz
-console.log(
-    `Einnahmen: ${einnahmen} ct
-    Ausgaben: ${ausgaben} ct
-    Bilanz: ${positive}`
-
-);
+};
+gesamt_auszufuhren();
