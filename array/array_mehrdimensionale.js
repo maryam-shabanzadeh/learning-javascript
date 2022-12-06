344a297c7470638440d06b83b5c2b9d9b1265768
maryam <maryam.shabanzadeh@valinton.com>
2022-12-06 13:57:16 +0100

    array-mehrdimensional

---
+++ array/array_mehrdimensionale.js
@@ -0,0 +1,35 @@
+"use strict";
+/*
+let obst = [
+    "Apfel",
+    "Birnen",
+    "Bananen"
+];
+
+let gemuese = [
+    "Möhren",
+    "Sellerie",
+    "Kohl"
+];
+
+let brot = [
+    "Graubrot",
+    "schwarzbrot",
+    "Vollkornbrot"
+];
+
+let waren =[
+    obst,
+    gemuese,
+    brot
+];*/
+
+
+let waren = [
+    ["Apfel", "Birnen", "Bananen"],
+    ["Möhren", "Sellerie", "Kohl"],
+    ["Graubrot", "Schwarzbrot", "Vollkornbrot"]
+]
+console.log(waren[1][1]);
+
+console.log(waren[0][2]);
