939db6dd2bcd29ece0c068b2e03caaeaa51573de
maryam <maryam.shabanzadeh@valinton.com>
2022-12-06 13:35:31 +0100

    array_besonders_objekt

---
+++ array/array_besonders_objekt.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let produkte_array = [
+    "Bananen",
+    "Apfel",
+    "Schokolate",
+    "Brot",
+    "Muslie"
+];
+//letzte array kann man zugreifen
+console.log(produkte_array.length-1);
+
+console.log(produkte_array[produkte_array.length-1]);
+
+let produkt_object ={
+    0: "Bananen",
+    1: "Apfel",
+    2: "Schokolade",
+    3: "Brot",
+    4: "Muslie"
+}
+console.log(produkte_array);
+console.log(produkt_object);
