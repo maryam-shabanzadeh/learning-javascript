72d8db60d80196d3d4c0e1d42f640f6ba2e92a8b
maryam <maryam.shabanzadeh@valinton.com>
2022-12-06 10:27:53 +0100

    switch-anweisung-teil1

---
+++ kontrollstrukturen/switch-anweisung_1.js
@@ -0,0 +1,15 @@
+"use strict";
+switch(ausdruck){
+    case werte_1:
+        //auszuführend Code
+        break;
+    case werte_2:
+        //auszuführende Code
+        break;
+    case werte_3:
+        //auszuführende Code
+        break;
+    default:
+        break;
+    
+}
